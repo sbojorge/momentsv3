4667d32220101f2af50f03eab3887c5093561984
sbojorge <sara.bojorge@gmail.com>
2023-06-07 21:05:27 +0000

    add alerts for the user

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -22,6 +22,8 @@ function SignInForm() {
     });
     const {username, password} = signInData;
 
+    const [errors, setErrors] = useState({});
+
     const history = useHistory();
 
     const handleChange = (event) => {
@@ -37,7 +39,7 @@ function SignInForm() {
           await axios.post("/dj-rest-auth/login/", signInData);
           history.push("/");
         } catch (err) {
-
+          setErrors(err.response?.data);
         }
     };
 
@@ -60,6 +62,11 @@ function SignInForm() {
                                 onChange={handleChange}
                             />
                         </Form.Group>
+                        {errors.username?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
 
                         <Form.Group controlId="password">
                             <Form.Label className="d-none">Password</Form.Label>
@@ -72,12 +79,23 @@ function SignInForm() {
                                 onChange={handleChange}
                             />
                         </Form.Group>
+                        {errors.password?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>
+                                {message}
+                            </Alert>
+                        ))}
+
                         <Button
                             className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
                             type="submit"
                         >
                             Sign in
                         </Button>
+                        {errors.non_field_errors?.map((message, idx) => (
+                          <Alert key={idx} variant="warning" className="mt-3">
+                            {message}
+                          </Alert>
+                        ))}
                     </Form> 
 
                 </Container>
