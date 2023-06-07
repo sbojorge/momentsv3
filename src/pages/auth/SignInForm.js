ddca9c0fef141238a2620c2767cdc5943a32bcbc
sbojorge <sara.bojorge@gmail.com>
2023-06-07 20:53:49 +0000

    add functionality to the sign in form

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
 
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -8,67 +9,95 @@ import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
 function SignInForm() {
-//   Add your component logic here
+    const [signInData, setSignInData] = useState({
+        username: '',
+        password: '',
+    });
+    const {username, password} = signInData;
 
-  return (
-    <Row className={styles.Row}>
-      <Col className="my-auto p-0 p-md-2" md={6}>
-        <Container className={`${appStyles.Content} p-4 `}>
-          <h1 className={styles.Header}>sign in</h1>
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setSignInData({
+            ...signInData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+          await axios.post("/dj-rest-auth/login/", signInData);
+          history.push("/");
+        } catch (err) {
+
+        }
+    };
+
+
+    return (
+        <Row className={styles.Row}>
+            <Col className="my-auto p-0 p-md-2" md={6}>
+                <Container className={`${appStyles.Content} p-4 `}>
+                    <h1 className={styles.Header}>sign in</h1>
           
-          <Form>
-            <Form.Group controlId="username">
-                <Form.Label className="d-none">Username</Form.Label>
-                <Form.Control
-                  className={styles.Input}
-                  type="text"
-                  placeholder="Enter Username"
-                  name="username"
-                />
-            </Form.Group>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group controlId="username">
+                            <Form.Label className="d-none">Username</Form.Label>
+                            <Form.Control
+                                className={styles.Input}
+                                type="text"
+                                placeholder="Enter Username"
+                                name="username"
+                                value={username}
+                                onChange={handleChange}
+                            />
+                        </Form.Group>
 
-            <Form.Group controlId="password">
-                <Form.Label className="d-none">Password</Form.Label>
-                <Form.Control
-                  className={styles.Input} 
-                  type="password"
-                  placeholder="Password"
-                  name="password"
-                />
-            </Form.Group>
-            <Button
-              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
-              type="submit"
+                        <Form.Group controlId="password">
+                            <Form.Label className="d-none">Password</Form.Label>
+                            <Form.Control
+                                className={styles.Input} 
+                                type="password"
+                                placeholder="Password"
+                                name="password"
+                                value={password}
+                                onChange={handleChange}
+                            />
+                        </Form.Group>
+                        <Button
+                            className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+                            type="submit"
+                        >
+                            Sign in
+                        </Button>
+                    </Form> 
+
+                </Container>
+                <Container className={`mt-3 ${appStyles.Content}`}>
+                    <Link className={styles.Link} to="/signup">
+                      Don't have an account? <span>Sign up now!</span>
+                    </Link>
+                </Container>
+            </Col>
+            <Col
+              md={6}
+              className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
             >
-              Sign in
-            </Button>
-          </Form> 
-
-        </Container>
-        <Container className={`mt-3 ${appStyles.Content}`}>
-          <Link className={styles.Link} to="/signup">
-            Don't have an account? <span>Sign up now!</span>
-          </Link>
-        </Container>
-      </Col>
-      <Col
-        md={6}
-        className={`my-auto d-none d-md-block p-2 ${styles.SignInCol}`}
-      >
-        <Image
-          className={`${appStyles.FillerImage}`}
-          src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero.jpg"}
-        />
-      </Col>
-    </Row>
-  );
+                <Image
+                    className={`${appStyles.FillerImage}`}
+                    src={"https://codeinstitute.s3.amazonaws.com/AdvancedReact/hero.jpg"}
+                />
+            </Col>
+        </Row>
+    );
 }
 
 export default SignInForm;
