09854c0ad660be5e6316365c053f6e6fba7f1de6
sbojorge <sara.bojorge@gmail.com>
2023-06-07 09:11:52 +0000

    add hooks to the form and alert in the username field

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,20 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
+import axios from "axios";
 
 const SignUpForm = () => {
+    const [signUpData, setSignUpData] = useState({
+        username: '',
+        password1: '',
+        password2: ''
+    });
+    const {username, password1, password2} = signUpData;
+
+    const [errors, setErrors] = useState({});
+
+    const history = useHistory();
+
+    const handleChange = (event) => {
+        setSignUpData({
+            ...signUpData,
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post('/dj-rest-auth/registration/', signUpData);
+            history.push('/signin');
+        } catch (err) {
+          setErrors(err.response?.data);
+        }
+    };
+
     return (
         <Row className={styles.Row}>
             <Col className="my-auto py-2 p-md-2" md={6}>
                 <Container className={`${appStyles.Content} p-4 `}>
                     <h1 className={styles.Header}>sign up</h1>
 
-                    <Form>
+                    <Form onSubmit={handleSubmit}>
                         <Form.Group controlId="username">
                             <Form.Label className="d-none">Username</Form.Label>
                             <Form.Control
@@ -22,8 +51,14 @@ const SignUpForm = () => {
                                 type="text"
                                 placeholder="Username"
                                 name="username"
+                                value={username}
+                                onChange={handleChange}
                             />
                         </Form.Group>
+                        {errors.username?.map((message, idx) => (
+                            <Alert variant="warning" key={idx}>{message}</Alert>
+
+                        ))}
 
                         <Form.Group controlId="password1">
                             <Form.Label className="d-none">Password</Form.Label>
@@ -32,6 +67,8 @@ const SignUpForm = () => {
                                 type="password"
                                 placeholder="Password"
                                 name="password1"
+                                value={password1}
+                                onChange={handleChange}
                             />
                         </Form.Group>
 
@@ -42,6 +79,8 @@ const SignUpForm = () => {
                                 type="password"
                                 placeholder="Confirm password"
                                 name="password2"
+                                value={password2}
+                                onChange={handleChange}
                             />
                         </Form.Group>
 
