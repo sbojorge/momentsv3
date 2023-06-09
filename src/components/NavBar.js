1d813155d2f38e88382fa4c7c6eaefc2837f28d0
sbojorge <sara.bojorge@gmail.com>
2023-06-09 10:40:58 +0000

    add a custom hook to handle toggling of the burger menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,11 +7,14 @@ import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContex
 
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
 
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
+    
   const handleSignOut = async () => {
     try {
       await axios.post("dj-rest-auth/logout/");
@@ -83,7 +86,7 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar expanded={expanded} className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -91,7 +94,11 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addPostIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref ={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
             <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
