d364e97fcf767f03170849f0180f8d8151852b00
sbojorge <sara.bojorge@gmail.com>
2023-06-10 07:31:08 +0000

    Add input fields to the add post form

--- src/pages/posts/PostCreateForm.js
+++ src/pages/posts/PostCreateForm.js
@@ -17,13 +17,42 @@ function PostCreateForm() {
 
   const [errors, setErrors] = useState({});
 
+  const [postData, setPostData] = useState({
+    title: '',
+    content: '',
+    image: '',
+  });
+  const { title, content, image } = postData;
+
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
   const textFields = (
     <div className="text-center">
-      {/* Add your form fields here */}
-
-    
-    
+      <Form.Group>
+        <Form.Label>Title</Form.Label>
+        <Form.Control
+          type="text"
+          name="title"
+          value={title}
+          onChange={handleChange}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label>Content</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={6}
+          name="content"
+          value={content}
+          onChange={handleChange}
+        />
+      </Form.Group>
+     
       <Button
         className={`${btnStyles.Button} ${btnStyles.Blue}`}
         onClick={() => {}}
@@ -64,4 +93,8 @@ function PostCreateForm() {
   );
 }
 
-export default PostCreateForm;
+export default PostCreateForm;  
+   
+      
+
+  
