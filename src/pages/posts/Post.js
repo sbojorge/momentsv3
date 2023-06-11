cdbe485e1ae3f6c4fbefdb8576ffadf15c6dfcc0
sbojorge <sara.bojorge@gmail.com>
2023-06-11 20:06:10 +0000

    create the Post.js file and import it as a component in PostPage.js

---
+++ src/pages/posts/Post.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../../styles/Post.module.css';
+
+const Post = (props) => {
+  return (
+    <div>Post placeholder text</div>
+  )
+}
+
+export default Post
--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -7,6 +7,7 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
 
 function PostPage() {
   const { id } = useParams();
@@ -16,7 +17,7 @@ function PostPage() {
     const handleMount = async () => {
       try {
         const [{ data: post }] = await Promise.all([
-            axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/posts/${id}`),
         ]);
         setPost({ results: [post] });
         console.log(post);
@@ -32,10 +33,8 @@ function PostPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Post component</p>
-        <Container className={appStyles.Content}>
-          Comments
-        </Container>
+        <Post {...post.results[0]} setPosts={setPost} />
+        <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
