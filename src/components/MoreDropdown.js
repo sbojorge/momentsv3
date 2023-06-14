70a3ce58fb81dab1f2d67a59464e23c976082ad4
sbojorge <sara.bojorge@gmail.com>
2023-06-14 15:16:58 +0000

    add the edit and delete post  functionality

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -16,7 +16,7 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
     />
   ));
 
-  export const MoreDropdown = () => {
+  export const MoreDropdown = ({ handleEdit, handleDelete }) => {
     return (
       <Dropdown className="ml-auto" drop="left">
         <Dropdown.Toggle as={ThreeDots} />
@@ -25,14 +25,14 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
         <Dropdown.Menu className="text-center" >
           <Dropdown.Item
            className={styles.DropdownItem}
-           onClick={() => {}}
+           onClick={handleEdit}
            aria-label="edit"
           >
             <i className="fas fa-edit"/>
           </Dropdown.Item>
           <Dropdown.Item
             className={styles.DropdownItem}
-            onClick={() => {}}
+            onClick={handleDelete}
             aria-label="delete"
           >
             <i className="fas fa-trash-alt"/>
--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from '../../styles/Post.module.css';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { Card, Media, OverlayTrigger, Tooltip } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import Avatar from '../../components/Avatar';
 import { axiosRes } from '../../api/axiosDefaults';
 import { MoreDropdown } from '../../components/MoreDropdown';
@@ -25,7 +25,21 @@ const Post = (props) => {
   } = props;
 
   const currentUser = useCurrentUser();
-  const is_owner = currentUser?.username === owner
+  const is_owner = currentUser?.username === owner;
+  const history = useHistory();
+
+  const handleEdit = () => {
+    history.push(`/posts/${id}/edit`);
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/posts/${id}/`);
+      history.goBack();
+    } catch(err){
+      console.log(err);
+    }
+  };
 
   const handleLike = async () => {
     try {
@@ -69,7 +83,12 @@ const Post = (props) => {
           </Link>
           <div className="d-flex align-items-center">
             <span>{updated_at}</span>
-            {is_owner && postPage && <MoreDropdown />}
+            {is_owner && postPage && (
+              <MoreDropdown 
+                handleEdit={handleEdit} 
+                handleDelete={handleDelete}
+              />
+            )}
           </div>
         </Media>
       </Card.Body>
