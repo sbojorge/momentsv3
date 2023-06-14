731416ecf3cfe0eee1937be04d87489221718ffe
sbojorge <sara.bojorge@gmail.com>
2023-06-14 20:55:17 +0000

    create the Comment  component to display the existing comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -8,6 +8,7 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import Comment from "../comments/Comment";
 
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
@@ -24,11 +25,12 @@ function PostPage() {
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: post }] = await Promise.all([
+        const [{ data: post }, { data: comments}] = await Promise.all([
           axiosReq.get(`/posts/${id}`),
+          axiosReq.get(`/comments/?post=${id}`),
         ]);
         setPost({ results: [post] });
-        console.log(post);
+        setComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -49,10 +51,21 @@ function PostPage() {
               profileImage={profile_image}
               post={id}
               setPost={setPost}
+              setComments={setComments}
             />
           ) : comments.results.length ? (
             "Comments"
-          ) : null} 
+          ) : null}
+          {comments.results.length ? (
+            comments.results.map(comment => (
+              <Comment key={comment.id} {...comment} />
+            ))
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments...yet</span>
+
+          )} 
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
