555e52725d60ab1de1a44917efcb90fbcc3aca25
sbojorge <sara.bojorge@gmail.com>
2023-06-19 20:56:10 +0000

    fix errors logged in the console

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,6 +8,7 @@ import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContex
 import Avatar from "./Avatar";
 import axios from "axios";
 import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
+import { removeTokenTimestamp } from "../utils/utils";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
@@ -19,6 +20,7 @@ const NavBar = () => {
     try {
       await axios.post("dj-rest-auth/logout/");
       setCurrentUser(null);
+      removeTokenTimestamp
     } catch (err) {
       console.log(err);
     }
--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -16,6 +16,7 @@ import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 import { useRedirect } from "../../hooks/useRedirect";
+import { setTokenTimestamp } from "../../utils/utils";
 
 function SignInForm() {
     const setCurrentUser = useSetCurrentUser();
@@ -40,9 +41,11 @@ function SignInForm() {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+
         try {
           const {data} = await axios.post("/dj-rest-auth/login/", signInData);
-          setCurrentUser(data.user)
+          setCurrentUser(data.user);
+          setTokenTimestamp(data);
           history.goBack();
         } catch (err) {
           setErrors(err.response?.data);
